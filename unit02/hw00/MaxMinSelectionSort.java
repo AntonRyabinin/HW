6ddb5d227b000f66316ba1f893fc64d44b915d5a
Anton Ryabinin <35781390+AntonRyabinin@users.noreply.github.com>
2018-02-02 02:26:08 +0300

    Add files via upload

---
+++ unit02/hw00/MaxMinSelectionSort.java
@@ -0,0 +1,144 @@
+package by.htp.jd01.less04.hw00;
+
+import java.util.Scanner;
+
+public class MaxMinSelectionSort {
+
+    @SuppressWarnings("resource")
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+        double[] arr = enterDoubleArray();
+        show(arr, "\nArray before sort: ");
+
+        double[] arrSorted = selectionSort(arr);
+        show(arrSorted,"\nArray after sort: ");
+
+        double[] arrIndexMin = minValue(arrSorted);
+        double max = maxValue(arrSorted);
+
+        double min = arrIndexMin[0];
+        double iMin = arrIndexMin[1];
+
+        System.out.printf("\nIndex min: %.0f\n", iMin);
+        showValue(min, "Min: ");
+        showValue(max, "Max: ");
+    }
+
+
+
+    private static double[] enterDoubleArray() {
+
+        int n = 0;
+
+        System.out.print("Enter array length: ");
+        while(sc.hasNext()) {
+            if (sc.hasNextInt()) {
+                n = sc.nextInt();
+                if (n >= 2) {
+                    break;
+                } else {
+                    System.out.println("Array length cannot be < 2");
+                    System.out.print("Re-enter array length: ");
+                }
+            } else {
+                sc.next();
+                System.out.print("Not integer! Re-enter array length: ");
+            }
+        }
+
+        double[] arrDouble = new double[n];
+
+        for(int i = 0; i < n; i++){
+            System.out.print("Enter arr[" + i + "]: ");
+            while (!sc.hasNextDouble()) {
+                sc.next();
+                System.out.print("Not double! Re-enter arr[" + i + "]: ");
+            }
+            arrDouble[i] = sc.nextDouble();
+        }
+
+        return arrDouble;
+    }
+
+
+
+    private static double[] selectionSort(double[] arrDouble) {
+
+        int min = 0;
+        double tmp = 0;
+
+        for (int i = 0; i < arrDouble.length - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < arrDouble.length; j++) {
+                if (arrDouble[j] < arrDouble[min]) {
+                    min = j;
+                }
+            }
+
+            tmp = arrDouble[min];
+            arrDouble[min] = arrDouble[i];
+            arrDouble[i] = tmp;
+        }
+
+        return arrDouble;
+    }
+
+
+
+    private static double[] minValue(double[] arr){
+
+        double min = arr[0];
+        double[] arrMinIndex = new double[2];
+        double tmp = 0;
+
+        for(int i = 0; i< arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+                tmp = i;
+            }
+        }
+
+        arrMinIndex[0] = min;
+        arrMinIndex[1] = tmp;
+
+        return arrMinIndex;
+    }
+
+
+
+    public static double maxValue(double[] arr){
+
+        double max = arr[0];
+
+        for(int i = 0; i< arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+
+
+    private static void show(double[] arr, String msg){
+
+        System.out.println(msg);
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+
+
+    private static void showValue(double val, String msg){
+
+        System.out.println(msg + val);
+
+    }
+}
